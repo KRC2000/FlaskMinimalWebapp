51bfb49860e6895eadb2ac8e9db8a49a08190ff1
Roman Kovalov <pomkov2000@gmail.com>
2023-08-27 12:18:43 +0300

    PUT request processing added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,4 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-    
+@app.route('/', methods=['PUT'])
+def put():
+    if request.method == 'PUT':
+        data = request.get_json()
+        if data is not None:
+            print(data)
+            return 'Data received and processed'
+        else:
+            return 'No data provided', 400
