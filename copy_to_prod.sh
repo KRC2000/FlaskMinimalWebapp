aa174cf2c6217adc81bbd7f0167033b3e3a0ef77
Roman Kovalov <pomkov2000@gmail.com>
2023-09-03 22:40:53 +0300

    Fixed path

--- copy_to_prod.sh
+++ copy_to_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ----- Job started: copy_to_prod.sh -------------------------------------------------
 
-if [ ! -d "FlaskMinimalWebapp_prod" ]; then
+if [ ! -d "../FlaskMinimalWebapp_prod" ]; then
 	echo INFO: FlaskMinimalWebapp_prod folder absent, cloning
 	git clone https://KRC2000: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@github.com/KRC2000/FlaskMinimalWebapp_prod.git ..
 fi
